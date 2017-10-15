32a517edd7195c877fbc82c6529abee6d71bc4b3
WilfredLemus <wilfred.lean.15@gmail.com>
2017-10-14 22:50:31 -0600

    ChatBot IdentifyImage

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  messenger: {
+    access_token: 'EAAGxktiF7eABAL6Jz8OsfaoGFDBcNdO8aVfLSWBhXzROKNDWBiz9IRstSSZB3CfMhoPcyWojRvQ9b1KmPDU2qIa2mgA3QTEths7ydEJHpJR6EU7FEK2GKiOWcDuTlvrQiaH0JKvS6SwqvuyGO8LOQ1AHqZAoEOGkR9JmspyZBadrMJUNt7M',
+    verify_token: 'f8A10pnmKFuLt2nD7iDwaB2PeUEbLD5O',
+  },
+  apiKeyMicrosoft: 'd25d513c52d041a2a5b0031ee2a2c77b'
+}
